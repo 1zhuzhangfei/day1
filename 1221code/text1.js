2d4ef8bdddc5ae5dd3f9d40170182eb2265adf95
“1zhuzhangfei” <1299160553@qq.com>
2024-12-24 17:35:06 +0800

    1224代码提交

---
+++ 1221code/text1.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            height: {
+                '6055': '6055px',
+                '1428': '1428px',
+            },
+            width: {
+                '1920': '1920px'
+            },
+        }
+    }
+}
